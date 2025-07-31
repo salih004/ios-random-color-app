92d48987bd2e01b17d571e2caedf4dd61c24e001
salih004 <145712148+salih004@users.noreply.github.com>
2025-07-31 15:05:38 -0500

    Initial Commit

---
+++ IOS_Prework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS_Prework
+//
+//  Created by Salih Mohamed on 7/31/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
